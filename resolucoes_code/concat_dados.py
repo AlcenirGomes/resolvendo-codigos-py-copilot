71fa8d9b84645ba84c0e3d08d557b2d215a73b48
Alcenir Gomes da Costa <alcenir.g.costa@hotmail.com>
2025-05-25 12:13:01 -0300

    feat: Criação de arquivos py, para resolução de códigos

---
+++ resolucoes_code/concat_dados.py
@@ -0,0 +1,11 @@
+# Vamos receber dois dados diferentes do usuário e concatená-los em uma única string.
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+# Concatenando as informações
+info_concatenada = info1 + " " + info2
+
+# Exibindo a informação concatenada
+print("A informação concatenada é:", info_concatenada)
+
